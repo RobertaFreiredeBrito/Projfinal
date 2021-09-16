ffdc82495489f9f2bed38cdd5d41414d650fe69c
RobertaFreiredeBrito <robertafb2011@gmail.com>
2021-09-16 20:01:18 -0300

    connect banco de dados

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
-const connectToDB = require("./database/database");
-const routes = require ("./routes/routes")
-
+const connectToDB = require("../database/database");
+const routes = require ("../routes/routes")
+connectToDB()
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
+app.use(routes);
+
 
 app.listen(port, () => console.info(`Servidor rodando em http://localhost:${port}`)
 )
